8d802149ca9265c23d9c4abd77212534b53bcddd
Arihant1467 <arihantsai.paruchuru@sjsu.edu>
2019-04-15 17:42:43 -0700

    struct changes for dahsboard

--- dashboardapi/src/dashboardapi/types.go
+++ dashboardapi/src/dashboardapi/types.go
@@ -5,14 +5,14 @@ import "gopkg.in/mgo.v2/bson"
 type Dashboard struct{
 	Id      	bson.ObjectId 	`json:"id" bson:"_id,omitempty"`
 	UserId		string		  	`json:"_id" bson:"_id,omitempty"`
-	events		[]Event			`json:"event" bson:"event,omitempty"`
+	Events		[]Event			`json:"event" bson:"event,omitempty"`
 }
 
 type Event struct {
     Id      	bson.ObjectId 	 `json:"id" bson:"_id,omitempty"`
-	numberOfBookings	int		 `json:"numberOfBookings" bson:"numberOfBookings,omitempty"`
-	numberOfViews		int		 `json:"numberOfViews" bson:"numberOfViews,omitempty"`
-	location			string   `json:"location" bson:"location"`
-	stillOpen			bool     `json:"stillOpen" bson:"stillOpen"`
+	NumberOfBookings	int		 `json:"numberOfBookings" bson:"numberOfBookings,omitempty"`
+	NumberOfViews		int		 `json:"numberOfViews" bson:"numberOfViews,omitempty"`
+	Location			string   `json:"location" bson:"location"`
+	StillOpen			bool     `json:"stillOpen" bson:"stillOpen"`
 }
 
