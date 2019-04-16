bafeb9de47a3626ee6fcbd2de66420cf5a3c0e1a
pratikb25 <pratik.bhandarkar@sjsu.edu>
2019-04-15 19:20:31 -0700

    added resource to GET details for given user ID

--- userapi/user/server.go
+++ userapi/user/server.go
@@ -8,7 +8,7 @@ package main
 import (
 	// "log"
 	"encoding/json"
-	// "fmt"
+	//"fmt"
 	"net/http"
 
 	"github.com/codegangsta/negroni"
@@ -18,15 +18,21 @@ import (
 
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-
 	// "net"
 	// "os"
 	// "strings"
 )
 
-var mongodb_server = os.Getenv("MONGO_SERVER")
+/*var mongodb_server = os.Getenv("MONGO_SERVER")
 var mongodb_database = os.Getenv("MONGO_DATABASE")
 var mongodb_collection = os.Getenv("MONGO_COLLECTION")
+var mongo_admin_database = os.Getenv("MONGO_ADMIN_DATABASE")
+var mongo_username = os.Getenv("MONGO_USERNAME")
+var mongo_password = os.Getenv("MONGO_PASS")*/
+
+var mongodb_server = "localhost"
+var mongodb_database = "userdb"
+var mongodb_collection = "users"
 
 func newUserServer() *negroni.Negroni {
 	formatter := render.New(render.Options{
@@ -42,7 +48,7 @@ func newUserServer() *negroni.Negroni {
 /* Initializing resource URI */
 func initRoutes(router *mux.Router, formatter *render.Render) {
 	router.HandleFunc("/users", getAllUsers).Methods("GET")
-	// router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
 	// router.HandleFunc("/users/signup", createUser).Methods("POST")
 	// router.HandleFunc("/users/signin", userSignIn).Methods("POST")
 	// router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
@@ -83,8 +89,9 @@ func getAllUsers(w http.ResponseWriter, req *http.Request) {
 
 	session.SetMode(mgo.Monotonic, true)
 
-	var result []User
+	var result []User // Result set to store records
 
+	/* Open DB connection*/
 	c := session.DB(mongodb_database).C(mongodb_collection)
 
 	query := bson.M{} // Empty query to fetch all records
@@ -92,7 +99,7 @@ func getAllUsers(w http.ResponseWriter, req *http.Request) {
 	err = c.Find(query).All(&result) // Fetch all users
 
 	if err != nil {
-		message := struct{ Message string }{"Users not found"}
+		message := struct{ Message string }{"Error while fetching users"}
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(message)
 		return
@@ -101,14 +108,15 @@ func getAllUsers(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
-/* Handler for /user/{id} */
-/*func getUser(w http.ResponseWriter, req *http.Request) {
+/**
+ * Fetch record of given user
+ * Handler for /user/{id}
+ */
+func getUser(w http.ResponseWriter, req *http.Request) {
 	setupResponse(&w, req)
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(req)
-	var m User
-	_ = json.NewDecoder(req.Body).Decode(&m)
-	fmt.Println("Get data of user: ", params["id"])
+
+	/* Open DB session */
 	session, err := mgo.Dial(mongodb_server)
 	if err != nil {
 		message := struct{ Message string }{"Some error occured while connecting to database!!"}
@@ -116,33 +124,51 @@ func getAllUsers(w http.ResponseWriter, req *http.Request) {
 		json.NewEncoder(w).Encode(message)
 		return
 	}
-	err = session.DB(mongo_admin_database).Login(mongo_username, mongo_password)
-	if err != nil {
-		message := struct{ Message string }{"Some error occured while login to database!!"}
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(message)
-		return
-	}
+
 	defer session.Close()
+
 	session.SetMode(mgo.Monotonic, true)
-	c := session.DB(mongodb_database).C(mongodb_collection)
-	query := bson.M{"id": params["id"]}
+
+	params := mux.Vars(req)
+
+	var id string = params["id"]
+
+	query := bson.M{"id": id}
+
 	var result bson.M
-	err = c.Find(query).One(&result)
-	if err != nil && err != mgo.ErrNotFound {
-		message := struct{ Message string }{"Some error occured while querying to database!!"}
-		w.WriteHeader(http.StatusNotFound)
+
+	if id == "" {
+		message := struct{ Message string }{"User ID not provided"}
+		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(message)
 		return
-	} else if err == mgo.ErrNotFound {
-		message := struct{ Message string }{"User not found"}
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(message)
-		return
+	} else {
+		/* Obtain DB connection */
+		c := session.DB(mongodb_database).C(mongodb_collection)
+
+		/* Execute query */
+		err = c.Find(query).One(&result)
+
+		/* Handle errors and return apt response*/
+		if err != nil && err != mgo.ErrNotFound {
+			text := "Error while fetching user information" + id
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(text)
+			return
+		}
+
+		if err != nil && err == mgo.ErrNotFound {
+			text := "No user found for ID: " + id
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(text)
+			return
+		}
 	}
+
 	json.NewEncoder(w).Encode(result)
 }
 
+/*
 func createUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var person User
@@ -189,8 +215,9 @@ func createUser(w http.ResponseWriter, req *http.Request) {
 	}
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(person)
-}
+}*/
 
+/*
 func deleteUser(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(req)
