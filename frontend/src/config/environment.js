0ebaac41bc282e981cd1f1610bc0b5f1722dc0a3
mssrinivas <saypatil12345@gmail.com>
2019-04-22 18:00:07 -0700

    adding bucketname for booked events

--- frontend/src/config/environment.js
+++ frontend/src/config/environment.js
@@ -1,10 +1,10 @@
-var eventURL= 'http://10.0.0.27:3000/'
+var eventURL= 'http://3.209.144.62:3000/'
 var userURL= 'http://50.112.231.74:3000/'
-var goURL= 'http://localhost:3000/'
+var bookURL= 'http://10.0.0.234:3000/'
 
 
 export{
-    goURL,
+    bookURL,
     userURL,
     eventURL
 }
