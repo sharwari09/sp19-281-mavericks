629781a1787b7523413c7cf881fa8255521d9abe
mssrinivas <saypatil12345@gmail.com>
2019-04-23 00:20:23 -0700

    GO API for getting booked events by userID and eventID

--- bookeventapi/src/goapi/server.go
+++ bookeventapi/src/goapi/server.go
@@ -11,7 +11,7 @@ import (
 	"github.com/unrolled/render"
 	// "github.com/satori/go.uuid"
 	"gopkg.in/mgo.v2"
-    // "gopkg.in/mgo.v2/bson"
+    "gopkg.in/mgo.v2/bson"
 )
 
 // MongoDB Config
@@ -36,7 +36,8 @@ func initRoutes(mx *mux.Router, formatter *render.Render) {
 	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
 	mx.HandleFunc("/book", bookHandler(formatter)).Methods("POST")
 	mx.HandleFunc("/book", optionsHandler(formatter)).Methods("OPTIONS")
-	mx.HandleFunc("/booking", bookingsHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/booking/{userID}", getuserBookings(formatter)).Methods("GET")
+	mx.HandleFunc("/bookings/{eventID}", geteventBookings(formatter)).Methods("GET")
 }
 
 // Helper Functions
@@ -63,13 +64,13 @@ func optionsHandler(formatter *render.Render) http.HandlerFunc {
 }
 
 func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://10.0.0.234:3000")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
     (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
     (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
-// API Process Orders 
+// API to book an event 
 func bookHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		var b Bookings
@@ -106,11 +107,12 @@ func bookHandler(formatter *render.Render) http.HandlerFunc {
 		formatter.JSON(w, http.StatusOK, struct{ Response string }{"Event successfully booked"})
 	}
 }
-func bookingsHandler(formatter *render.Render) http.HandlerFunc {
+
+// API to get booked event by userID
+func getuserBookings(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusOK, struct{ Test string }{"get bookings API alive!"})
 		setupResponse(&w, req)
-		fmt.Println("inside getbookingsHandler")
+		fmt.Println("inside getuserBookings")
 		
 		session, err := mgo.Dial(mongodb_server)
         if err != nil {
@@ -121,11 +123,10 @@ func bookingsHandler(formatter *render.Render) http.HandlerFunc {
         c := session.DB(mongodb_database).C(mongodb_collection)
 
         var results []Bookings
-        // params := mux.Vars(req)
-        // var userID string = params["userID"]
-        // fmt.Printf( "User ID: %s", userID)
-        // err = c.Find(bson.M{"userID": userID}).All(&results)
-        err = c.Find(nil).All(&results)
+        params := mux.Vars(req)
+        var userID string = params["userID"]
+        fmt.Printf( "User ID: %s", userID)
+        err = c.Find(bson.M{"userID": userID}).All(&results)
         if err != nil {
         		fmt.Println("Error while getting booked events: ", err)
                 log.Fatal(err)
@@ -136,6 +137,47 @@ func bookingsHandler(formatter *render.Render) http.HandlerFunc {
         response := EventResponse{
 			Count: len(results),
 			BookedEvents: results}
+
+		if len(results) > 0 {
+			formatter.JSON(w, http.StatusOK, response)
+		}else{
+			formatter.JSON(w, http.StatusNoContent,
+				struct{ Response string }{"No booked Events found"})
+		}
+	}
+}
+
+// API to get booked event by eventID
+
+func geteventBookings(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		setupResponse(&w, req)
+		fmt.Println("inside geteventBookings")
+		
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+
+        var results []Bookings
+        params := mux.Vars(req)
+        var eventID string = params["eventID"]
+        fmt.Printf( "Event ID: %s", eventID)
+        err = c.Find(bson.M{"eventID": eventID}).All(&results)
+        if err != nil {
+        		fmt.Println("Error while getting booked events: ", err)
+                log.Fatal(err)
+                formatter.JSON(w, http.StatusInternalServerError, 
+				struct{ Response error }{err})
+        }
+        fmt.Println(results)
+        response := EventResponse{
+			Count: len(results),
+			BookedEvents: results}
+
 		if len(results) > 0 {
 			formatter.JSON(w, http.StatusOK, response)
 		}else{
