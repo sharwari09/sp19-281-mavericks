190357dc38d6e6d9b2fcb25636b192befba4190d
pratikb25 <pratik.bhandarkar@sjsu.edu>
2019-04-30 14:41:40 -0700

    POST new event details to dashboard

--- eventsapi/src/events/types.go
+++ eventsapi/src/events/types.go
@@ -2,27 +2,27 @@ package main
 
 import "time"
 
-type EventPayload struct{
-	OrgId        string   `json:"orgId" bson:"orgId"`
-	EventId   string    `json:"eventId" bson:"eventId"`
-	EventName string	`json:"eventName" bson:"eventName"`
-	Location string		`json:"location" bson:"location"`
-	Date string		`json:"date" bson:"date"`
-	BucketName string 	`json:"bucketname" bson:"bucketname"`
-	Price float32 		`json:"price"`
+type EventPayload struct {
+	OrgId      string  `json:"orgId" bson:"orgId"`
+	EventId    string  `json:"eventId" bson:"eventId"`
+	EventName  string  `json:"eventName" bson:"eventName"`
+	Location   string  `json:"location" bson:"location"`
+	Date       string  `json:"date" bson:"date"`
+	BucketName string  `json:"bucketname" bson:"bucketname"`
+	Price      float32 `json:"price"`
 }
 
-type ScheduledEvent struct{
-	OrgId        string   `json:"orgId"`
-	EventId   string    `json:"eventId" bson:"eventId"`
-	EventName string	`json:"eventName" bson:"eventName"`
-	Location string		`json:"location" bson:"location"`
-	Date time.Time		`json:"date" bson:"date"`
-	BucketName string 	`json:"bucketname" bson:"bucketname"`
-	Price float32 		`json:"price"`
+type ScheduledEvent struct {
+	OrgId      string    `json:"orgId"`
+	EventId    string    `json:"eventId" bson:"eventId"`
+	EventName  string    `json:"eventName" bson:"eventName"`
+	Location   string    `json:"location" bson:"location"`
+	Date       time.Time `json:"date" bson:"date"`
+	BucketName string    `json:"bucketname" bson:"bucketname"`
+	Price      float32   `json:"price"`
 }
 
-type EventResponse struct{
-	Count	int		`json:"count"`
-	Events  []ScheduledEvent		`json:"events"`
+type EventResponse struct {
+	Count  int              `json:"count"`
+	Events []ScheduledEvent `json:"events"`
 }
