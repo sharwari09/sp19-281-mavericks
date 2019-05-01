09154d0713a404f4d913fdd4508b7505501c5104
Thol Chidambaram <tholkappia@gmail.com>
2019-05-01 11:51:53 -0700

    modified the structure

--- dashboardlambda/incrementViewUserEvent/incrementViewUserEvent.go
+++ dashboardlambda/incrementViewUserEvent/incrementViewUserEvent.go
@@ -16,7 +16,7 @@ import (
 type MyRequest struct {
 	Bucket  string `json:"bucket"`
 	Key     string `json:"user_uuid"` // The organizer of the event
-	EventID string `json:"eventId"`   // The event
+	EventID string `json:"eventId"`   // Organizer of the event
 
 }
 
@@ -33,7 +33,8 @@ type Dashboard struct {
 }
 
 type PostedEvent struct {
-	OrgID            string `json:"orgId"`
+	OrgID            string `json:"orgId"`   // id of the organizer
+	EventID          string `json:"eventId"` // id of the event
 	EventName        string `json:"eventName"`
 	Location         string `json:"location"`
 	Date             string `json:"date"`
@@ -44,6 +45,7 @@ type PostedEvent struct {
 type BookedEvent struct {
 	OrgID         string `json:"orgId"`
 	EventName     string `json:"eventName"`
+	EventID       string `json:"eventId"`
 	Date          string `json:"date"`
 	TimeOfBooking string `json:"timeOfBooking"`
 	Location      string `json:"location"`
@@ -53,13 +55,15 @@ func incrementViewUserEvent(request MyRequest) (MyResponse, error) {
 
 	var nlb = os.Getenv("riak_cluster_nlb")
 	var port = os.Getenv("nlb_port")
-	// Unboxing Request
+
 	var bucket = request.Bucket
 	var key = request.Key
 	var eventID = request.EventID
 
 	// Getting the details of the user from RIAK
+	fmt.Println("Arihant")
 	var url = fmt.Sprintf("http://%s:%s/buckets/%s/keys/%s", nlb, port, bucket, key)
+	fmt.Println(url)
 	responseUserDetails, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("ERROR: %s", err)
@@ -67,30 +71,26 @@ func incrementViewUserEvent(request MyRequest) (MyResponse, error) {
 	}
 	defer responseUserDetails.Body.Close()
 	responseUserDetailsBody, _ := ioutil.ReadAll(responseUserDetails.Body)
-
 	var dashboard Dashboard
 	errResponseUserDetailsBody := json.Unmarshal(responseUserDetailsBody, &dashboard)
-
 	if errResponseUserDetailsBody != nil {
 		log.Fatalf("ERROR: %s", errResponseUserDetailsBody)
 		return MyResponse{Status: false}, errResponseUserDetailsBody
 	}
 
 	for i := range dashboard.PostedEvents {
-		if dashboard.PostedEvents[i].OrgID == eventID {
-			views := dashboard.PostedEvents[i].NumberOfViews
-			dashboard.PostedEvents[i].NumberOfViews = views + 1
-			fmt.Println("In break statement")
+		if dashboard.PostedEvents[i].EventID == eventID {
+			numberofViews := dashboard.PostedEvents[i].NumberOfViews
+			dashboard.PostedEvents[i].NumberOfViews = numberofViews + 1
 			break
 		}
 	}
 
 	dashboardString, _ := json.Marshal(dashboard)
-
+	fmt.Println(responseUserDetailsBody)
 	payload := bytes.NewBuffer(dashboardString)
 	req, _ := http.NewRequest("PUT", url+"?returnbody=true", payload)
 	req.Header.Add("Content-Type", "application/json")
-
 	responsePostingEvent, errPostingEvent := http.DefaultClient.Do(req)
 
 	if errPostingEvent != nil {
@@ -103,6 +103,7 @@ func incrementViewUserEvent(request MyRequest) (MyResponse, error) {
 	body, _ := ioutil.ReadAll(responsePostingEvent.Body)
 
 	fmt.Printf("Results: %s\n", string(body))
+
 	return MyResponse{Status: true}, nil
 
 }
@@ -116,9 +117,9 @@ func main() {
 API Gateway URL:
 # request
 {
-	Bucket  string `json:"bucket"`
-	Key     string `json:"user_uuid"` // The organizer of the event
-	EventID string `json:"eventId"`   // The event
+  "bucket": "eventbrite",
+  "user_uuid": "a418b7f2-1aec-4e70-a0c7-984fc12ff587",
+  "eventId": "806ef8b7-8261-459e-903e-0abed74ee"
 }
 
 
@@ -138,5 +139,5 @@ riak_cluster_nlb riak-cluster-network-lb-d90a8ac266b9ee92.elb.us-west-2.amazonaw
   "user_uuid": "4a6bd3f7-9fa9-44e7-a79c-2258594fe0c6",
   "eventId": "8ff53536-95df-42c5-8f31-8bf7733f2f7e"
 }
-
+a418b7f2-1aec-4e70-a0c7-984fc12ff587
 */
