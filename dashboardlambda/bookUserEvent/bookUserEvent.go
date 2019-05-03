a4aada9d6ad354d5970c429a014e252d2910145c
Arihant1467 <arihantsai.paruchuru@sjsu.edu>
2019-05-03 09:32:10 -0700

    added a call for increment Views for booking

--- dashboardlambda/bookUserEvent/bookUserEvent.go
+++ dashboardlambda/bookUserEvent/bookUserEvent.go
@@ -17,10 +17,12 @@ type MyRequest struct {
 	Bucket        string `json:"bucket"`
 	Key           string `json:"user_uuid"` // The one who is booking the event
 	OrgID         string `json:"orgId"`     // Organizer of the event
+	EventID       string `json:"eventId"`   // The event to be booked
 	EventName     string `json:"eventName"`
 	Date          string `json:"date"`
 	TimeOfBooking string `json:"timeOfBooking"`
 	Location      string `json:"location"`
+	Price         int    `json:"price"` // price
 }
 
 // AWS lambda response
@@ -36,36 +38,49 @@ type Dashboard struct {
 }
 
 type PostedEvent struct {
-	OrgID            string `json:"orgId"`
+	OrgID            string `json:"orgId"`   // id of the organizer
+	EventID          string `json:"eventId"` // id of the event
 	EventName        string `json:"eventName"`
 	Location         string `json:"location"`
 	Date             string `json:"date"`
 	NumberOfViews    int    `json:"numberOfviews"`
 	NumberOfBookings int    `json:"numberOfBookings"`
+	Price            int    `json:"price"` // price
 }
 
 type BookedEvent struct {
 	OrgID         string `json:"orgId"`
 	EventName     string `json:"eventName"`
+	EventID       string `json:"eventId"`
 	Date          string `json:"date"`
 	TimeOfBooking string `json:"timeOfBooking"`
 	Location      string `json:"location"`
+	Price         int    `json:"price"` // price
+}
+
+type IncrementBooking struct {
+	Bucket  string `json:"bucket"`
+	Key     string `json:"user_uuid"`
+	EventID string `json:"eventId"`
 }
 
 func bookUserEvent(request MyRequest) (MyResponse, error) {
 
 	var nlb = os.Getenv("riak_cluster_nlb")
 	var port = os.Getenv("nlb_port")
-	// Unboxing Request
 	var bucket = request.Bucket
 	var key = request.Key
 	var orgID = request.OrgID
+	var eventID = request.EventID
 	var eventName = request.EventName
 	var date = request.Date
 	var timeOfBooking = request.TimeOfBooking
 	var location = request.Location
+	var price = request.Price
 
 	// Getting the details of the user from RIAK
+	fmt.Printf("Request Object\n")
+	fmt.Printf("%v\n", request)
 	var url = fmt.Sprintf("http://%s:%s/buckets/%s/keys/%s", nlb, port, bucket, key)
 	responseUserDetails, err := http.Get(url)
 	if err != nil {
@@ -83,12 +98,15 @@ func bookUserEvent(request MyRequest) (MyResponse, error) {
 
 	bookEvent := BookedEvent{
 		OrgID:         orgID,
+		EventID:       eventID,
 		EventName:     eventName,
 		Date:          date,
 		TimeOfBooking: timeOfBooking,
 		Location:      location,
+		Price:         price,
 	}
 
+	// Adding to list of booked Events
 	dashboard.BookedEvents = append(dashboard.BookedEvents, bookEvent)
 	dashboardString, _ := json.Marshal(dashboard)
 
@@ -106,9 +124,35 @@ func bookUserEvent(request MyRequest) (MyResponse, error) {
 	defer responsePostingEvent.Body.Close()
 	body, _ := ioutil.ReadAll(responsePostingEvent.Body)
 
+	go incrementBookingOfEvent(orgID, eventID, bucket)
 	fmt.Printf("Results: %s\n", string(body))
 	return MyResponse{Status: true}, nil
 
+}
+
+func incrementBookingOfEvent(orgID string, eventID string, bucket string) {
+	fmt.Println("****Increment booking event****")
+	bookingIncrementURL := os.Getenv("booking_increment_url")
+	fmt.Println(bookingIncrementURL)
+	data := IncrementBooking{
+		Bucket:  bucket,
+		Key:     orgID,
+		EventID: eventID,
+	}
+	//data := map[string]string{"bucket": bucket, "user_uuid": orgID, "eventId": eventID}
+	payload, _ := json.Marshal(data)
+	var jsonStr = []byte(payload)
+	req, _ := http.NewRequest("POST", bookingIncrementURL, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Content-Type", "application/json")
+	response, err := http.DefaultClient.Do(req)
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("The error is")
+		fmt.Println(err.Error())
+	}
+
+	defer response.Body.Close()
+	fmt.Println("****Increment booking event end****")
 }
 
 func main() {
@@ -120,13 +164,14 @@ func main() {
 API Gateway URL:
 # request
 {
-	Bucket        string `json:"bucket"`
-	Key           string `json:"user_uuid"` // The one who is booking the event
-	OrgID         string `json:"orgId"`     // Organizer of the event
-	EventName     string `json:"eventName"`
-	Date          string `json:"date"`
-	TimeOfBooking string `json:"timeOfBooking"`
-	Location      string `json:"location"`
+	"bucket": "eventbrite",
+  "user_uuid": "4a6bd3f7-9fa9-44e7-a79c-2258594fe0c",
+  "orgId": "d4a03e22-2055-4167-8209-aaad98f29bcc",
+  "eventId" : "",
+  "eventName": "Arihant's Birthday",
+  "date": "27-06-2019",
+  "timeOfBooking": "25-04-2019",
+  "location": "Brooklyn,NY"
 }
 
 
